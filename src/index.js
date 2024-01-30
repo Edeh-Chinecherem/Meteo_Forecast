2042c55ebebd3ab0daf789641dbabe50b9ad483f
Chinecherem Edeh <edehchinecherem08@gmail.com>
2024-01-30 16:59:28 +0100

    Inject HTML forecast from JavaScript

--- src/index.js
+++ src/index.js
@@ -55,8 +55,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="row">
+                    <div class="col-2">
+                        <div class="weather-forecast-date">${day}
+                        </div>
+
+                        <div class="weather-forecast-icon">🌧 </div>
+                        <div class="weather-forecast-temperature">
+                            <span class="weather-forecast-temperature-max">18° </span>
+                            <span class="weather-forecast-temperature-min">12°</span>
+                        </div>
+
+                    </div>
+                </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
